13afe315e30f11255689b0058ff0a6421e99a89d
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2024-03-07 21:13:11 +0530

    Update test_zsd_single_img.sh

--- mmdetection/test_zsd_single_img.sh
+++ mmdetection/test_zsd_single_img.sh
@@ -1,5 +1,3 @@
-#./tools/dist_test_single_img.sh configs/pascal_voc/faster_rcnn_r101_fpn_1x_voc0712_single_img.py ./work_dirs/pascal_voc_t2/epoch_4.pth 1 --dataset voc --out ./work_dirs/pascal_voc_workdirs/single_img.pkl --gzsd --syn_weights ./work_dirs/pascal_voc_t2/GAN_outputs/classifier_best.pth 
-#./tools/dist_test_single_img.sh configs/faster_rcnn_r101_fpn_1x _single_img.py ./work_dirs/coco2014/epoch_12.pth 1 --dataset coco --out ./work_dirs/single_img.pkl --gzsd --syn_weights /workspace/arijit_ug/sushil/zsd/checkpoints/coco_65_15_wgan_modeSeek_seen_cycSeenUnseen_triplet_varMargin_try21/classifier_best_latest.pth 
 ./tools/dist_test_single_img.sh configs/pascal_voc/faster_rcnn_r101_fpn_1x_voc0712_single_img.py ./work_dirs/voc/epoch_4.pth 1 --dataset voc --out ./work_dirs/single_img.pkl --gzsd --syn_weights workspace/arijit_ug/sushil/zsd/checkpoints/voc/VOC_wgan_seen_cyclicSeenUnseen_triplet_varMar_try8/classifier_best_latest.pth 
 
 #/workspace/arijit_ug/sushil/zsd/checkpoints/voc/VOC_wgan_seen_cyclicSeenUnseen_triplet_varMar_try8
